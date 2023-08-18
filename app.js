61fe930af8b6fdde4b40d2d7f982ab82f15b8d1f
vrajt <vrajthaker10@gmail.com>
2023-08-18 10:42:50 +0530

    Start

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ else{
     console.log("its bigger number")
 }
 console.log("my first program")
+
+const names=require('./utils.js')
+const Greet=require('./name.js')
+const data=require('./AlternativeFlavor.js')
+const add=require('./MindGraned.js')
+console.log(names)
+Greet(names.john);
+console.log(data)
+require('./MindGraned.js')
+//thats it
